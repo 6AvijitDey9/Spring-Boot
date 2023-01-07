e2b3ead4c5eb3db7b915d0994f9093432e624846
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2023-01-07 06:32:40 -0800

    Delete Springrst/Springrst directory

--- Springrst/Springrst/src/main/java/com/springrest/Springrst/Dao/Dao.java
+++
@@ -1,7 +0,0 @@
-package com.springrest.Springrst.Dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.springrest.Springrst.entity.Course;
-
-public interface Dao extends JpaRepository<Course,Long> {
-
-}
